3d194e7faa2718dd2221a3c08c8a7623d72035e7
AndrewI26 <andrewi44@icloud.com>
2026-09-12 16:39:36 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::io;
+
+fn main() {
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+
+    let operations = ['+', '-', '/', '*'];
+
+    println!("Enter the first number: ");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Failed to read first number");
+
+    println!("Enter the second number: ");
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Failed to read second number");
+
+    let num1: f64 = num1.trim().parse().expect("Invalid first number");
+    let num2: f64 = num2.trim().parse().expect("Invalid second number");
+
+    println!("Both numbers read successfully!");
+
+    println!("Enter an operation (+, -, /, *):");
+    let mut operation = String::new();
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("Failed to read second number");
+
+    let operation = operation.trim().chars().next().unwrap();
+
+    if operations.contains(&operation) {
+        println!("Valid operation: {}", operation);
+    } else {
+        println!("Invalid operation!");
+    }
+
+    let result = match operation {
+        '+' => num1 + num2,
+        '-' => num1 + num2,
+        '*' => num1 + num2,
+        '/' => num1 + num2,
+        _ => unreachable!(),
+    };
+
+    println!("Result: {}", result);
+}
