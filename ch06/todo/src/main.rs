c3ec23650119ba7e80d551877c0fa69c9a8e8e33
AndrewI26 <andrewi44@icloud.com>
2026-09-13 19:58:55 -0400

    Input handling improvments

--- ch06/todo/src/main.rs
+++ ch06/todo/src/main.rs
@@ -14,14 +14,20 @@ enum Action {
 }
 
 impl Action {
-    fn get_command(&self) -> String {
+    fn get_command(&self) -> char {
         match self {
-            Action::List => String::from("L"),
-            Action::Complete => String::from("C"),
-            Action::Delete => String::from("D"),
-            Action::Add => String::from("A"),
-            Action::Quit => String::from("Q"),
+            Action::List => 'L',
+            Action::Complete => 'C',
+            Action::Delete => 'D',
+            Action::Add => 'A',
+            Action::Quit => 'Q',
         }
+    }
+
+    fn from_char(c: char) -> Option<Action> {
+        Action::all()
+            .into_iter()
+            .find(|action| action.get_command() == c.to_ascii_uppercase())
     }
 
     fn all() -> [Action; 5] {
@@ -48,7 +54,7 @@ impl TodoList {
             println!(
                 "{}. [{}] {}",
                 i + 1,
-                if todo.is_complete { "✓" } else { "X" },
+                if todo.is_complete { "✓" } else { " " },
                 todo.name
             )
         });
@@ -59,14 +65,41 @@ impl TodoList {
             is_complete: false,
         });
     }
-    fn delete(&mut self, num: u8) {
-        self.todos.remove((num + 1).into());
+    fn delete(&mut self, num: usize) -> bool {
+        if num >= 1 && num <= self.todos.len() {
+            self.todos.remove(num - 1);
+            true
+        } else {
+            false
+        }
     }
-    fn complete(&mut self, num: u8) {
-        for (i, todo) in self.todos.iter_mut().enumerate() {
-            if i - 1 == num.into() {
+    fn complete(&mut self, num: usize) -> bool {
+        match num.checked_sub(1).and_then(|i| self.todos.get_mut(i)) {
+            Some(todo) => {
                 todo.is_complete = !todo.is_complete;
+                true
             }
+            None => false,
+        }
+    }
+}
+
+fn read_line() -> Option<String> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(0) | Err(_) => None,
+        Ok(_) => Some(input.trim().to_string()),
+    }
+}
+
+fn read_number() -> Option<usize> {
+    println!("Please enter the number of a todo: ");
+    let input = read_line()?;
+    match input.parse() {
+        Ok(number) => Some(number),
+        Err(_) => {
+            println!("Invalid number {input}");
+            None
         }
     }
 }
@@ -77,63 +110,50 @@ fn main() {
     let mut todo_list = TodoList::new();
 
     loop {
-        let mut input = String::new();
         println!(
             "Please type a command ({}): ",
             Action::all()
                 .iter()
-                .map(|action| action.get_command())
+                .map(|action| action.get_command().to_string())
                 .collect::<Vec<String>>()
                 .join(", ")
         );
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read command");
 
-        let ch = input.trim().chars().next().unwrap();
+        // End of input (e.g. Ctrl+D) quits
+        let Some(input) = read_line() else { break };
+        let Some(ch) = input.chars().next() else {
+            continue;
+        };
 
-        match ch {
-            'L' => todo_list.list(),
-            'C' => {
-                println!("Please enter the number of a todo: ");
-                let mut input = String::new();
+        let Some(action) = Action::from_char(ch) else {
+            println!("Invalid command {ch}");
+            continue;
+        };
 
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read number");
-
-                let number: u8 = input.parse().expect("Invalid number");
-
-                todo_list.complete(number);
+        match action {
+            Action::List => todo_list.list(),
+            Action::Complete => {
+                if let Some(number) = read_number() {
+                    if !todo_list.complete(number) {
+                        println!("No todo with number {number}");
+                    }
+                }
             }
-            'A' => {
+            Action::Add => {
                 println!("Please enter the name of the new todo: ");
-                let mut input = String::new();
+                let Some(name) = read_line() else { break };
 
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read number");
-
-                todo_list.add(input);
+                todo_list.add(name);
                 todo_list.list();
             }
-            'D' => {
-                println!("Please enter the number of a todo: ");
-                let mut input = String::new();
-
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read number");
-
-                let number: u8 = input.parse().expect("Invalid number");
-
-                todo_list.delete(number);
+            Action::Delete => {
+                if let Some(number) = read_number() {
+                    if !todo_list.delete(number) {
+                        println!("No todo with number {number}");
+                    }
+                }
             }
-            'Q' => break,
-            _ => {
-                println!("Invalid command {ch}");
-                continue;
-            }
+            Action::Quit => break,
         }
     }
 }
